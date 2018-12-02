e6b52352f8594d6371f5e7a978b2f906e66792ca
LorranRamos <ptklorran@gmail.com>
2018-12-02 00:20:15 -0200

    adicionei a página de login

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { Button } from 'react-materialize'
+
+export default class Login extends Component {
+  render() {
+    return (
+        <div>
+            <Button>Olá Mundo</Button>
+        </div>
+    );
+  }
+}
