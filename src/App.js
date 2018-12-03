797e7526c1d0482a0deb63dc54f48914a9886cce
LorranRamos <ptklorran@gmail.com>
2018-12-03 14:53:26 -0200

    reset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react';
 
-import Login from './pages/Login'
-import Home from './pages/Home'
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/login" component={Login} />
-          </Switch>
-        </Router>
-      </div>
     );
   }
 }
-
-export default App;
--- src/services/Firebase.js
+++ src/services/Firebase.js
@@ -1,13 +1,6 @@
 import Firebase from 'firebase'
 
-const config = {
-    apiKey: "AIzaSyDbjpUGPz2Tesks2cm7mteRw9kg3l0-99k",
-    authDomain: "bonsaiproducoes-pacheco.firebaseapp.com",
-    databaseURL: "https://bonsaiproducoes-pacheco.firebaseio.com",
-    projectId: "bonsaiproducoes-pacheco",
-    storageBucket: "bonsaiproducoes-pacheco.appspot.com",
-    messagingSenderId: "237959077398"
-}
+import config from './config'
 
 export const firebase = Firebase.initializeApp(config)
 export const DB = firebase.database()
